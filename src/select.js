ef8427eb1513d74f7001bacfba8fb969178826f1
William Troup <williamtroup@gmail.com>
2023-12-08 11:57:09 +0000

    Added core code for extending, and updated gitignore.

---
+++ src/select.js
@@ -0,0 +1,395 @@
+/**
+ * Select.js
+ * 
+ * package.json
+ * 
+ * @file        select.js
+ * @version     v0.1.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2023
+ */
+
+
+( function() {
+    var // Variables: Constructor Parameters
+        _parameter_Document = null,
+        _parameter_Navigator = null,
+        _parameter_Window = null,
+
+        // Variables: Configuration
+        _configuration = {},
+
+        // Variables: Strings
+        _string = {
+            empty: "",
+            space: " "
+        },
+
+        // Variables: Elements
+        _elements_Type = {},
+        _elements = [],
+
+        // Variables: Attribute Names
+        _attribute_Name_Options = "data-select-options";
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Rendering
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function render() {
+        var tagTypes = _configuration.highlightAllDomElementTypes,
+            tagTypesLength = tagTypes.length;
+
+        for ( var tagTypeIndex = 0; tagTypeIndex < tagTypesLength; tagTypeIndex++ ) {
+            var domElements = _parameter_Document.getElementsByTagName( tagTypes[ tagTypeIndex ] ),
+                elements = [].slice.call( domElements ),
+                elementsLength = elements.length;
+
+            for ( var elementIndex = 0; elementIndex < elementsLength; elementIndex++ ) {
+                if ( !renderElement( elements[ elementIndex ] ) ) {
+                    break;
+                }
+            }
+        }
+    }
+
+    function renderElement( element ) {
+        var result = true;
+
+        if ( isDefined( element ) && element.hasAttribute( _attribute_Name_Options ) ) {
+            var bindingOptions = element.getAttribute( _attribute_Name_Options );
+
+            if ( isDefinedString( bindingOptions ) ) {
+
+            } else {
+                if ( !_configuration.safeMode ) {
+                    console.error( "The attribute '" + _attribute_Name_Options + "' has not been set correctly." );
+                    result = false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Options
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function buildAttributeOptions( newOptions ) {
+        var options = !isDefinedObject( newOptions ) ? {} : newOptions;
+        options.showCopyButton = getDefaultBoolean( options.showCopyButton, true );
+        
+        options = buildAttributeOptionStrings( options );
+
+        return buildAttributeOptionCustomTriggers( options );
+    }
+
+    function buildAttributeOptionStrings( options ) {
+        options.copyButtonText = getDefaultString( options.copyButtonText, "Copy" );
+
+        return options;
+    }
+
+    function buildAttributeOptionCustomTriggers( options ) {
+        options.onCopy = getDefaultFunction( options.onCopy, null );
+
+        return options;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Validation
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function isDefined( value ) {
+        return value !== null && value !== undefined && value !== _string.empty;
+    }
+
+    function isDefinedObject( object ) {
+        return isDefined( object ) && typeof object === "object";
+    }
+
+    function isDefinedBoolean( object ) {
+        return isDefined( object ) && typeof object === "boolean";
+    }
+
+    function isDefinedString( object ) {
+        return isDefined( object ) && typeof object === "string";
+    }
+
+    function isDefinedFunction( object ) {
+        return isDefined( object ) && typeof object === "function";
+    }
+
+    function isDefinedNumber( object ) {
+        return isDefined( object ) && typeof object === "number";
+    }
+
+    function isDefinedArray( object ) {
+        return isDefinedObject( object ) && object instanceof Array;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Element Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function createElement( type, className ) {
+        var result = null,
+            nodeType = type.toLowerCase(),
+            isText = nodeType === "text";
+
+        if ( !_elements_Type.hasOwnProperty( nodeType ) ) {
+            _elements_Type[ nodeType ] = isText ? _parameter_Document.createTextNode( _string.empty ) : _parameter_Document.createElement( nodeType );
+        }
+
+        result = _elements_Type[ nodeType ].cloneNode( false );
+
+        if ( isDefined( className ) ) {
+            result.className = className;
+        }
+
+        return result;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Triggering Custom Events
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function fireCustomTrigger( triggerFunction ) {
+        if ( isDefinedFunction( triggerFunction ) ) {
+            triggerFunction.apply( null, [].slice.call( arguments, 1 ) );
+        }
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Default Parameter/Option Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function getDefaultString( value, defaultValue ) {
+        return isDefinedString( value ) ? value : defaultValue;
+    }
+
+    function getDefaultBoolean( value, defaultValue ) {
+        return isDefinedBoolean( value ) ? value : defaultValue;
+    }
+
+    function getDefaultFunction( value, defaultValue ) {
+        return isDefinedFunction( value ) ? value : defaultValue;
+    }
+
+    function getDefaultArray( value, defaultValue ) {
+        return isDefinedArray( value ) ? value : defaultValue;
+    }
+
+    function getDefaultNumber( value, defaultValue ) {
+        return isDefinedNumber( value ) ? value : defaultValue;
+    }
+
+    function getDefaultStringOrArray( value, defaultValue ) {
+        if ( isDefinedString( value ) ) {
+            value = value.split( _string.space );
+
+            if ( value.length === 0 ) {
+                value = defaultValue;
+            }
+
+        } else {
+            value = getDefaultArray( value, defaultValue );
+        }
+
+        return value;
+    }
+
+    function getObjectFromString( objectString ) {
+        var parsed = true,
+            result = null;
+
+        try {
+            if ( isDefinedString( objectString ) ) {
+                result = JSON.parse( objectString );
+            }
+
+        } catch ( e1 ) {
+
+            try {
+                result = eval( "(" + objectString + ")" );
+
+                if ( isDefinedFunction( result ) ) {
+                    result = result();
+                }
+                
+            } catch ( e2 ) {
+                if ( !_configuration.safeMode ) {
+                    console.error( "Errors in object: " + e1.message + ", " + e2.message );
+                    parsed = false;
+                }
+                
+                result = null;
+            }
+        }
+
+        return [ parsed, result ];
+    }
+
+    function getClonedObject( object ) {
+        var json = JSON.stringify( object ),
+            result = JSON.parse( json );
+
+        return result;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * String Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function newGuid() {
+        var result = [];
+
+        for ( var charIndex = 0; charIndex < 32; charIndex++ ) {
+            if ( charIndex === 8 || charIndex === 12 || charIndex === 16 || charIndex === 20 ) {
+                result.push( "-" );
+            }
+
+            var character = Math.floor( Math.random() * 16 ).toString( 16 );
+            result.push( character );
+        }
+
+        return result.join( _string.empty );
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public Functions:  Destroying
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * destroyAll().
+     * 
+     * Reverts all rendered elements back to their original state (without render attributes).
+     * 
+     * @public
+     * 
+     * @returns     {Object}                                                The Select.js class instance.
+     */
+    this.destroyAll = function() {
+        return this;
+    };
+
+    /**
+     * destroy().
+     * 
+     * Reverts an element back to its original state (without render attributes).
+     * 
+     * @public
+     * 
+     * @param       {string}    elementId                                   The ID of the DOM element to destroy.
+     * 
+     * @returns     {Object}                                                The Select.js class instance.
+     */
+    this.destroy = function( elementId ) {
+        return this;
+    };
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public Functions:  Configuration
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * setConfiguration().
+     * 
+     * Sets the specific configuration options that should be used.
+     * 
+     * @public
+     * 
+     * @param       {Options}   newConfiguration                            All the configuration options that should be set (refer to "Options" documentation for properties).
+     * 
+     * @returns     {Object}                                                The Select.js class instance.
+     */
+    this.setConfiguration = function( newOptions ) {
+        _configuration = !isDefinedObject( newOptions ) ? {} : newOptions;
+        
+        buildDefaultConfiguration();
+
+        return this;
+    };
+
+    function buildDefaultConfiguration() {
+        _configuration.safeMode = getDefaultBoolean( _configuration.safeMode, true );
+        _configuration.highlightAllDomElementTypes = getDefaultStringOrArray( _configuration.highlightAllDomElementTypes, [ "div", "code" ] );
+        _configuration.maximumButtons = getDefaultNumber( _configuration.maximumButtons, 2 );
+        _configuration.buttonsVisible = getDefaultBoolean( _configuration.buttonsVisible, true );
+        _configuration.buttonsOpenerText = getDefaultString( _configuration.buttonsOpenerText, "<" );
+        _configuration.buttonsCloserText = getDefaultString( _configuration.buttonsCloserText, ">" );
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public Functions:  Additional Data
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * getVersion().
+     * 
+     * Returns the version of Select.js.
+     * 
+     * @public
+     * 
+     * @returns     {string}                                                The version number.
+     */
+    this.getVersion = function() {
+        return "0.1.0";
+    };
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Initialize Select.js
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    ( function ( documentObject, navigatorObject, windowObject ) {
+        _parameter_Document = documentObject;
+        _parameter_Navigator = navigatorObject;
+        _parameter_Window = windowObject;
+
+        buildDefaultConfiguration();
+
+        _parameter_Document.addEventListener( "DOMContentLoaded", function() {
+            render();
+        } );
+
+        if ( !isDefined( _parameter_Window.$select ) ) {
+            _parameter_Window.$select = this;
+        }
+
+    } ) ( document, navigator, window );
+} )();
