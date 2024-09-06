6ea1728488275b4cf7db233837b03eac93d46bc4
William Troup <williamtroup@gmail.com>
2024-09-06 14:30:10 +0100

    Added API interface.

---
+++ src/ts/api.ts
@@ -0,0 +1,68 @@
+/**
+ * Select.js
+ * 
+ * A lightweight, and easy-to-use, JavaScript library for creating multi-select drop-down lists!
+ * 
+ * @file        api.ts
+ * @version     v1.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2023
+ */
+
+
+export type PublicApi = {
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public API Functions:  Configuration
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+	/**
+	 * setConfiguration().
+	 *
+	 * Sets the specific configuration options that should be used.
+	 *
+	 * @public
+	 *
+	 * @param       {Options}   newConfiguration                            All the configuration options that should be set (refer to "Options" documentation for properties).
+	 *
+	 * @returns     {Object}                                                The Select.js class instance.
+	 */
+	setConfiguration: ( configuration: any ) => PublicApi;
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Public API Functions:  Additional Data
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+	/**
+	 * getIds().
+	 *
+	 * Returns an array of element IDs that have been rendered.
+	 *
+	 * @public
+	 *
+	 * @returns     {string[]}                                              The element IDs that have been rendered.
+	 */
+	getIds: () => string[];
+
+	/**
+	 * getVersion().
+	 *
+	 * Returns the version of Select.js.
+	 *
+	 * @public
+	 *
+	 * @returns     {string}                                                The version number.
+	 */
+	getVersion: () => string;
+};
+
+declare global {
+	interface Window {
+		$select: PublicApi;
+	}
+}
